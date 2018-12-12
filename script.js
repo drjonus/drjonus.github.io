f5aff6569cf02cf832bfe16f175f1e68c4260491
jeffrey <jeff.r.leslie@gmail.com>
2018-12-12 14:33:34 -0800

    yes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/MapID/997/256/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
-    accessToken: 'pk.eyJ1IjoiZHJqb251cyIsImEiOiJjanBsa3N6aDQwZG14NDNwYzM5bmNvd29sIn0.fu7aB5mOlZTslRUlJ5Uhtw'
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
 }).addTo(map);
-
-map.locate({setView: true, maxZoom: 16});
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
@@ -17,10 +17,11 @@ function onLocationFound(e) {
     L.circle(e.latlng, radius).addTo(map);
 }
 
-map.on('locationfound', onLocationFound);
-
 function onLocationError(e) {
     alert(e.message);
 }
 
+map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
+
+map.locate({setView: true, maxZoom: 16});
