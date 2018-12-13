aafd102bef47f908d6ff8fd2b8b55173cdcdf353
jeffrey <jeff.r.leslie@gmail.com>
2018-12-13 12:23:15 -0800

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function initialize() {
   function addMarker(e){
 	// Add marker to map at click location; add popup window
     newMarker = new L.marker(e.latlng).addTo(map);
-    newMarker.bindPopup(`{newMarker.getLatLng()}`).openPopup();
+    newMarker.bindPopup(`${newMarker.getLatLng()}`).openPopup();
   }
 
   map.on('locationfound', onLocationFound);
